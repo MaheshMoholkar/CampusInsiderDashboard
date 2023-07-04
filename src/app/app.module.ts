fe25b7f4d30912c68bf96333a1dd0fb5d9490a7d
Mahesh <moholkarmahesh@aca.edu.in>
2023-07-04 09:09:12 +0530

    Added Firebase and Firestore modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+
+import { environment } from 'src/environments/environment.prod';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './layouts/header/header.component';
 import { FooterComponent } from './layouts/footer/footer.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { CategoriesComponent } from './categories/categories.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    DashboardComponent
+    DashboardComponent,
+    CategoriesComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: 'true',
+  firebaseConfig: {
+    apiKey: 'AIzaSyAoBmC7LLdxyea_30_-BDEXD9RMTH-X3FE',
+    authDomain: 'ang-blog-app-f6621.firebaseapp.com',
+    projectId: 'ang-blog-app-f6621',
+    storageBucket: 'ang-blog-app-f6621.appspot.com',
+    messagingSenderId: '251512835754',
+    appId: '1:251512835754:web:fc94a38f2cee86ab0b8ad3',
+  },
+};
