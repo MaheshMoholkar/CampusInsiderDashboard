92aece7c8b486d9cf6343e48cd6262988897605c
Mahesh <moholkarmahesh@aca.edu.in>
2023-07-04 15:06:17 +0530

    Added Categories Component

--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 @Component({
   selector: 'app-categories',
   templateUrl: './categories.component.html',
-  styleUrls: ['./categories.component.css']
+  styleUrls: ['./categories.component.css'],
 })
-export class CategoriesComponent {
-
+export class CategoriesComponent implements OnInit {
+  constructor(private firestore: AngularFirestore) {}
+  ngOnInit(): void {}
+  onSubmit(formData: any) {
+    let categoryData = {
+      category: formData.value.category,
+    };
+    this.firestore
+      .collection('category')
+      .add(categoryData)
+      .then((docRef) => {
+        console.log(docRef);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 }
