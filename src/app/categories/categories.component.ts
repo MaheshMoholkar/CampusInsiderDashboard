ed9e92cb8cf5fefcfd933252f42377d3233e18ff
Mahesh <moholkarmahesh@aca.edu.in>
2023-07-05 13:54:53 +0530

    Categories Modifications

--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -8,13 +8,36 @@ import { Category } from '../models/category';
   styleUrls: ['./categories.component.css'],
 })
 export class CategoriesComponent implements OnInit {
+  categoryArray: Array<any> = [];
+  formCategory: string = '';
+  formStatus: string = 'Add';
+  categoryId: string = '';
   constructor(private categoryService: CategoriesService) {}
-  ngOnInit(): void {}
+
+  ngOnInit(): void {
+    this.categoryService.loadData().subscribe((val) => {
+      console.log(val);
+      this.categoryArray = val;
+    });
+  }
   onSubmit(formData: any) {
     let categoryData: Category = {
       category: formData.value.category,
     };
-
-    this.categoryService.saveData(categoryData);
+    if (this.formStatus == 'Add') {
+      this.categoryService.saveData(categoryData);
+    } else if (this.formStatus == 'Edit') {
+      this.categoryService.updateData(this.categoryId, categoryData);
+      this.formStatus = 'Add';
+    }
+    formData.reset();
+  }
+  onEdit(category: any, id: string) {
+    this.formCategory = category;
+    this.formStatus = 'Edit';
+    this.categoryId = id;
+  }
+  onDelete(id: any) {
+    this.categoryService.deleteData(id);
   }
 }
--- src/app/services/categories.service.ts
+++ src/app/services/categories.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { ToastrService } from 'ngx-toastr';
+import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -17,10 +18,43 @@ export class CategoriesService {
       .add(data)
       .then((docRef) => {
         console.log(docRef);
-        this.toastr.success('Category added successfully', 'Success');
+        this.toastr.success('Category Added Successfully', 'Success');
       })
       .catch((err) => {
         console.log(err);
+      });
+  }
+
+  loadData() {
+    return this.firestore
+      .collection('categories')
+      .snapshotChanges()
+      .pipe(
+        map((actions) => {
+          return actions.map((a) => {
+            const data = a.payload.doc.data();
+            const id = a.payload.doc['id'];
+            return { id, data };
+          });
+        })
+      );
+  }
+
+  updateData(id: any, EditData: any) {
+    this.firestore
+      .doc('categories/' + id)
+      .update(EditData)
+      .then((docRef) => {
+        this.toastr.success('Category Updated Successfully...');
+      });
+  }
+
+  deleteData(id: any) {
+    this.firestore
+      .doc('categories/' + id)
+      .delete()
+      .then((docRef) => {
+        this.toastr.warning('Category Deleted Successfully...');
       });
   }
 }
